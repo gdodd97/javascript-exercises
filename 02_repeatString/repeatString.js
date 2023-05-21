fa2e2a41c67fd836b23b3698e87a3d4dbafeb95f
Gregory Dodd <gdodd97@gmail.com>
2023-05-21 17:36:38 -0400

    Created repeat string function in repeatString.js. Passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+
+    let total_string = "";
+    if (num < 0){
+        return "ERROR";
+    }
+    else {
+        for (let i = 0; i < num; i++){
+            total_string += string; 
+        }
+        return total_string;
+    }
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
