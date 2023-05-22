3869dac2d5be2169b418def3d5140c7e27579309
Gregory Dodd <gdodd97@gmail.com>
2023-05-21 22:02:28 -0400

    Created sumAll function. Passed all tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function(first,second) {
+    //declare variables
+    let sum = 0;
+    let bigger = 0;
+    let smaller = 0;
 
+    //error handling
+    if (first < 0 || second < 0){
+        return "ERROR";
+    }
+    else if(!Number.isInteger(first) || !Number.isInteger(second)){
+        return "ERROR";
+    }
+    else{
+        //sort numbers in case first number is the larger number
+        if (first < second){
+            smaller = first;
+            bigger = second;
+        }
+        else{
+            bigger = first;
+            smaller = second;
+        }
+
+        for (i = smaller; i <= bigger; i++){
+            sum += i;
+        }    
+        return sum;
+    }
 };
 
 // Do not edit below this line
